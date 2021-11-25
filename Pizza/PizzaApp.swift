c453f8e42f8a91f7c7ea55b7cecda8bd616046ec
Jash Dhinoja <jash.dhinoja@gmail.com>
2021-11-25 18:42:20 -0500

    Initial Commit

---
+++ Pizza/PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaApp.swift
+//  Pizza
+//
+//  Created by pro on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
